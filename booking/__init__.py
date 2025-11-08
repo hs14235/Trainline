6eb98ee2474eee84cda8cbcf547b5b1aa56e0fb9
hs14235 <hamexpired@gmail.com>
2025-11-08 01:53:41 -0500

    fix: use venv python, add requests, safe pymysql import

--- booking/__init__.py
+++ booking/__init__.py
@@ -1,2 +1,6 @@
-import pymysql
-pymysql.install_as_MySQLdb()
+# booking/__init__.py
+try:
+    import pymysql  # only needed when using MySQL locally
+    pymysql.install_as_MySQLdb()
+except Exception:
+    pass
